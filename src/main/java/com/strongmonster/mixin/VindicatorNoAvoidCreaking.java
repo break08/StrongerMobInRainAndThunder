ec53111986a7e8d1e643dcc3b03f0e81661066be
NoName123 <dungluong3108@gmail.com>
2026-06-19 20:58:01 +0700

    Add goal mixins for Vindicator and Pillager
    Introduce VindicatorNoAvoidCreaking and convert the Pillager mixin to an AbstractIllager-based implementation that cancels registerGoals and re-adds the desired goal/target entries. Update the class tweaker to expose required classes/methods (Raider.HoldGroundAttackGoal, AbstractIllager.registerGoals, Vindicator inner goals) and add the new mixin entries to the mixin configuration, also reordering and cleaning up the mixin list.

--- src/main/java/com/strongmonster/mixin/VindicatorNoAvoidCreaking.java
+++ src/main/java/com/strongmonster/mixin/VindicatorNoAvoidCreaking.java
@@ -6,8 +6,9 @@ import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
 import net.minecraft.world.entity.animal.golem.IronGolem;
+import net.minecraft.world.entity.monster.creaking.Creaking;
 import net.minecraft.world.entity.monster.illager.AbstractIllager;
-import net.minecraft.world.entity.monster.illager.Pillager;
+import net.minecraft.world.entity.monster.illager.Vindicator;
 import net.minecraft.world.entity.npc.villager.AbstractVillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.raid.Raider;
@@ -17,26 +18,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Pillager.class)
-
-public abstract class PillagerNoAvoidCreaking extends AbstractIllager {
-    protected PillagerNoAvoidCreaking(EntityType<? extends Pillager> entityType, Level level) {
-      super(entityType, level);
+@Mixin(Vindicator.class)
+public abstract class VindicatorNoAvoidCreaking extends AbstractIllager {
+    protected VindicatorNoAvoidCreaking(EntityType<? extends Vindicator> entityType, Level level) {
+        super(entityType, level);
     }
-
     @Inject(method = "registerGoals", at = @At("HEAD"), cancellable = true)
     private void registerGoals (CallbackInfo ci){
         ci.cancel();
         super.registerGoals();
         this.goalSelector.addGoal(0, new FloatGoal(this));
-        this.goalSelector.addGoal(2, new Raider.HoldGroundAttackGoal(this, 10.0F));
-        this.goalSelector.addGoal(3, new RangedCrossbowAttackGoal(this, 1.0F, 8.0F));
-        this.goalSelector.addGoal(8, new RandomStrollGoal(this, 0.6));
-        this.goalSelector.addGoal(9, new LookAtPlayerGoal(this, Player.class, 15.0F, 1.0F));
-        this.goalSelector.addGoal(10, new LookAtPlayerGoal(this, Mob.class, 15.0F));
-        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this, Raider.class)).setAlertOthers());
+        this.goalSelector.addGoal(2, new Vindicator.VindicatorBreakDoorGoal(this));
+        this.goalSelector.addGoal(3, new AbstractIllager.RaiderOpenDoorGoal(this, this));
+        this.goalSelector.addGoal(4, new Raider.HoldGroundAttackGoal(this, 10.0F));
+        this.goalSelector.addGoal(5, new MeleeAttackGoal(this, (double)1.0F, false));
+        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this, new Class[]{Raider.class})).setAlertOthers(new Class[0]));
         this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
-        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, false));
+        this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, AbstractVillager.class, true));
         this.targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, IronGolem.class, true));
+        this.targetSelector.addGoal(4, new Vindicator.VindicatorJohnnyAttackGoal(this));
+        this.goalSelector.addGoal(8, new RandomStrollGoal(this, 0.6));
+        this.goalSelector.addGoal(9, new LookAtPlayerGoal(this, Player.class, 3.0F, 1.0F));
+        this.goalSelector.addGoal(10, new LookAtPlayerGoal(this, Mob.class, 8.0F));
     }
 }
